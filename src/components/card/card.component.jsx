4beced5797c8d5bb98b2b7359e6e33b36c609592
RussWooDev <russwoodev9@gmail.com>
2022-09-14 00:59:13 +0800

    first commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./card.styles.css";
+
+class Card extends Component {
+  render() {
+    const { id, email, name } = this.props.monster;
+    return (
+      <div className="card-container" key={id}>
+        <img
+          alt={`monster ${name}`}
+          src={`https://robohash.org/${id}/?set=set1&size=300x300`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
